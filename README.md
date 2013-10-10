c52f380641e6a60330e4a231810f8cb5e85b44b9
Travis Thompson <butters08@gmail.com>
2013-10-09 22:29:32 -0700

    Added zshrc, oh-my-zsh, and the dotfiles installer

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 dotfiles
 ========
+This is my repository for all of my configuration files that I use across machines.  It checks for updates and can be used to bootstrap a new system quickly.
+
+Installing
+----------
+Remember to setup ssh keys first (or use https for read only)
+```
+git clone git@github.com:aka-butters/dotfiles.git ~/.dotfiles
+./.dotfiles/dotfiles.sh
+```
+
+Pulling Updates
+---------------
+Once `~/.dotfiles/dotfiles.sh` is being sourced by `~/.zshrc` you can just run `dfup` to manually update.  It will also automatically update once a week.
+
+Pushing Updates
+---------------
+If a change is made to a non-local config file, you must push the update or updates won't pull down
+```
+cd ~/.dotfiles
+git commit -a
+git push
+```
---
+++ dotfiles.sh
@@ -0,0 +1,49 @@
+#!/bin/zsh
+[[ -z "$DF_DIR" ]] && DF_DIR=$HOME/.dotfiles
+[[ -z "$DF_UPCHECKTIME" ]] && DF_UPCHECKTIME=604800 # 1 week default
+DF_BLACKLIST+=("README.md" "dotfiles.sh")
+
+function dfinstall() {
+    for file in `ls $DF_DIR`; do
+        # Check if file is black listed
+        bl=false
+        for e in "${DF_BLACKLIST[@]}"; do 
+            if [[ "$file" = "$e" ]]; then 
+                bl=true; 
+            fi
+        done
+
+        if [[ "$bl" == "false" ]]; then
+            dfile=$file
+
+            # replace # with .
+            if [[ "${file[0,1]}" = "#" ]]; then
+                dfile=".${file[2,${#file}]}"
+            fi
+            if [ ! -L "$HOME/$dfile" ]; then
+                echo "### Installing $HOME/$dfile --> $DF_DIR/$file"
+                ln -sfn "$DF_DIR/$file" "$HOME/$dfile"
+            fi
+        fi
+    done
+}
+
+function dfup() {
+    echo "### Updating dotfiles..."
+    cd $DF_DIR
+    git pull
+    dfinstall
+    echo $(date +%s) > $DF_DIR/.lastup
+    echo "### done!"
+}
+
+if [ -f "$DF_DIR/.lastup" ]; then
+    read lastup < $DF_DIR/.lastup
+else
+    lastup=0
+fi
+((lastup = `date +%s` - lastup))
+
+if [ $lastup -ge $DF_UPCHECKTIME ]; then
+    dfup
+fi
