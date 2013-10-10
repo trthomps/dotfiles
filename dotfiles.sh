62e5d456efc55a1edf93d2d94b0c8da8261406a4
Travis Thompson <butters08@gmail.com>
2013-10-10 00:21:07 -0700

    Updated dotfiles to do commits

--- dotfiles.sh
+++ dotfiles.sh
@@ -31,13 +31,75 @@ function dfinstall() {
 function dfup() {
     cd $DF_DIR
     echo "### Checking for dotfile updates..."
+    
+    # Update remote branch
+    git remote update >/dev/null
+    
+    # See what we need to do
+    # If we're behind, hopefully no conflicts, you're on your own if there are
+    git log | grep behind >/dev/null
+    if [ $? -eq 0 ]; then
+        echo "### Updates found, pulling from github"
         git pull
-        dfinstall
+    fi
+
+    # If we have changes to commit, commit them then push them
+    if [[ "$DF_NOCOMMIT" == "true" ]]; then
+        echo "### No Commit Enabled, not checking for files to push"
+    else
+        # Check first for add
+        echo "### Looking for untracked files..."
+        git status -s --ignore-submodules | grep -E '^\?\?.+'
+        if [ $? -eq 0 ]; then
+            echo -n "### Do you want to add these files? If not consider adding them to .gitignore (y/N) "
+            read -sk addfiles
+            echo
+            if [[ $addfiles:l = y ]]; then
+                git add -A
+            fi
+        else
+            echo "### No untracked files found"
+        fi
+
+        # Check for modify/delete/new
+        echo "### Looking for new or modified files..." 
+        git status -s --ignore-submodules | grep -E '^( M|A ).+'
+        if [ $? -eq 0 ]; then
+            echo -n "### Do you want to commit these changes? (y/N) "
+            read -sk commitchanges
+            echo
+            if [[ $commitchanges:l = y ]]; then
+                git commit -a
+            fi
+        else
+            echo "### No changes found"
+        fi
+
+        # Check if push is needed
+        echo "### Checking if we're ahead of remote/origin..."
+        git status --ignore-submodules | grep ahead >/dev/null
+        if [ $? -eq 0 ]; then
+            echo -n "### Do you want to push your changes upstream? (y/N) "
+            read -sk pushchanges
+            echo
+            if [[ $pushchanges:l = y ]]; then
+                git push
+            fi
+        else
+            echo "### You're in sync with remote"
+        fi
+    fi
+
+    echo "### Running dfinstall..."
+    dfinstall
     echo "### done"
+    
     echo "### Checking for submodule updates..."
-        git submodule update --init --recursive
+    git submodule update --init --recursive
     echo "### done"
-    echo $(date +%s) > $DF_DIR/.lastup
+    
+    echo `date +%s` > $DF_DIR/.lastup
+    cd -
 }
 
 if [ -f "$DF_DIR/.lastup" ]; then
